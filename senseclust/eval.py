6eb5290d36fc549e4bd8d7daafe9039ba2dac3a8
Frankie Robertson <frankie@robertson.name>
2019-04-11 14:29:07 +0300

    Move some of eval into library code

---
+++ senseclust/eval.py
@@ -0,0 +1,62 @@
+from collections import Counter
+import click
+from senseclust.groupings import gen_groupings, gen_multi_groupings, inner_join, synset_key_clus
+from senseclust.utils import self_xtab
+from sklearn.feature_extraction.text import CountVectorizer
+
+HEADERS = ("pb,wn", "manann,ref")
+
+
+def calc_pr(tp, fp, fn):
+    # Copypaste from stiff
+    if tp == 0:
+        return 0, 0, 0
+    p = tp / (tp + fp)
+    r = tp / (tp + fn)
+    return (p, r, 2 * p * r / (p + r))
+
+
+def eval_clus(gold_clus, test_clus, cnt):
+    gold = synset_key_clus(gold_clus)
+    test = synset_key_clus(test_clus)
+    pairs = [(gold[gss], test[gss]) for gss in gold if gss in test]
+    for (g1, t1), (g2, t2) in self_xtab(pairs):
+        if g1 == g2:
+            if t1 == t2:
+                cnt['tp'] += 1
+            else:
+                cnt['fn'] += 1
+        else:
+            if t1 == t2:
+                cnt['fp'] += 1
+            else:
+                cnt['tn'] += 1
+
+
+def eval(gold, test, multi_group):
+    lemmas = 0
+    cnt = Counter()
+    line = next(gold)
+    assert line.strip() in HEADERS
+    if multi_group:
+        gold_gen = gen_multi_groupings(gold)
+    else:
+        gold_gen = gen_groupings(gold)
+    for lemma, gold_clus, test_clus in inner_join(gold_gen, gen_groupings(test)):
+        lemmas += 1
+        if multi_group:
+            for gc in gold_clus[0]:
+                eval_clus(gc, test_clus[0], cnt)
+        else:
+            eval_clus(gold_clus[0], test_clus[0], cnt)
+
+    p, r, f1 = calc_pr(cnt['tp'], cnt['fp'], cnt['fn'])
+    res = {}
+    res["lemmas"] = lemmas
+    res["cnt"] = cnt
+    res["pr"] = {
+        "p": p,
+        "r": r,
+        "f1": f1,
+    }
+    return res
