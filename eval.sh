9b66f891cd41e021fb49e654ca60fb63b467f232
Frankie Robertson <frankie@robertson.name>
2019-08-31 14:03:39 +0300

    Remvoe old entry points

--- eval.sh
+++
@@ -1,7 +0,0 @@
-for test in results/*
-do
-  echo "Concrete gold evaluation of $test"
-  poetry run python eval.py eval/frame-synset-union2.csv $test
-  echo "Synthetic gold evaluation of $test"
-  poetry run python eval.py --multi-group eval/synth-clus.csv $test
-done
--- tables.sh
+++
@@ -1,4 +0,0 @@
-echo "wordnet table"
-poetry run python expc.py sum-table --groups "gold:eval/frame-synset-union2.csv,eval/synth-clus.csv,eval/manclus.wn.csv" work/results/*.db "o,macc"
-echo "wiktionary table"
-poetry run python expc.py sum-table --groups "gold:eval/manclus.csv,eval/manclus.wiki.csv,eval/manclus.link.csv" work/results/*.db "o,macc"
