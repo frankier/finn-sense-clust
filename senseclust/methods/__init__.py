124a959d13cba498246b906f7dd94f76199f66d6
Frankie Robertson <frankie@robertson.name>
2019-10-27 10:43:54 +0200

    Remove unused stuff from method top level data

--- senseclust/methods/__init__.py
+++ senseclust/methods/__init__.py
@@ -1,17 +1,8 @@
-from senseclust.methods.label import label_graph, Label
-from senseclust.methods.vec_clust import vec_clust_autoextend_graph, Vec
-from senseclust.methods.gloss import gloss_graph, Gloss
+from senseclust.methods.label import Label
+from senseclust.methods.vec_clust import Vec
+from senseclust.methods.gloss import Gloss
 from senseclust.methods.baseline import Baseline
 from senseclust.methods.base import ExpGroup, WiktionaryExpGroup
-
-
-METHODS = {
-    "label-graph": label_graph,
-    "vec-clust-autoextend-graph": vec_clust_autoextend_graph,
-    "gloss-graph": gloss_graph,
-}
-
-SUPPORTS_WIKTIONARY = {"gloss-graph"}
 
 
 EXPERIMENTS = [
