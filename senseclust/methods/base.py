d9df10550276b2808dfe3366b45c38d4ad255f0a
Frankie Robertson <frankie@robertson.name>
2019-10-28 17:08:44 +0200

    Delete incomplete guesses in SenseClusExp to avoid misleading results

--- senseclust/methods/base.py
+++ senseclust/methods/base.py
@@ -1,11 +1,10 @@
+import os
 import sys
-from os.path import join as pjoin
 from dataclasses import dataclass
 
 from expcomb.models import Exp, ExpGroup as ExpGroupBase
 from senseclust.exceptions import NoSuchLemmaException
 from senseclust.eval import eval
-from wikiparse.utils.db import get_session
 from click.utils import LazyFile
 
 
@@ -23,25 +22,32 @@ class SenseClusExp(Exp):
     def run(self, words_fn, guess_fn, **extra):
         add_exemplars = getattr(self, "returns_centers", False) and extra.get("exemplars", False)
         with open(words_fn) as inf, LazyFile(guess_fn, "w") as outf:
-            for line in inf:
-                lemma_name, pos = line.strip().rsplit(",", 1)
-                try:
-                    if add_exemplars:
-                        clus_obj, centers = self.clus_lemma(lemma_name, pos, True)
+            try:
+                for line in inf:
+                    lemma_name, pos = line.strip().rsplit(",", 1)
+                    try:
+                        if add_exemplars:
+                            clus_obj, centers = self.clus_lemma(lemma_name, pos, True)
+                        else:
+                            clus_obj = self.clus_lemma(lemma_name, pos)
+                            centers = []
+                    except NoSuchLemmaException:
+                        print(f"No such lemma: {lemma_name}", file=sys.stderr)
                     else:
-                        clus_obj = self.clus_lemma(lemma_name, pos)
-                        centers = []
-                except NoSuchLemmaException:
-                    print(f"No such lemma: {lemma_name}", file=sys.stderr)
-                else:
-                    for k, v in sorted(clus_obj.items()):
-                        num = k + 1
-                        for ss in v:
-                            if add_exemplars:
-                                exemplar = "1" if ss in centers else "0"
-                                print(f"{lemma_name}.{num:02},{ss},{exemplar}", file=outf)
-                            else:
-                                print(f"{lemma_name}.{num:02},{ss}", file=outf)
+                        for k, v in sorted(clus_obj.items()):
+                            num = k + 1
+                            for ss in v:
+                                if add_exemplars:
+                                    exemplar = "1" if ss in centers else "0"
+                                    print(f"{lemma_name}.{num:02},{ss},{exemplar}", file=outf)
+                                else:
+                                    print(f"{lemma_name}.{num:02},{ss}", file=outf)
+            except Exception:
+                # This is probably a partial guess: delete it to avoid getting
+                # incorrect results
+                outf.close()
+                os.unlink(guess_fn)
+                raise
 
     def calc_score(self, gold, guess_path):
         return eval(open(gold), open(guess_path), False)
