30a084895662e6558b738e0e99f9e561343ceebf
Frankie Robertson <frankie@robertson.name>
2019-11-29 17:51:48 +0200

    Fix clipping

--- senseclust/utils.py
+++ senseclust/utils.py
@@ -89,7 +89,7 @@ def split_line(line):
 
 def cos_affinities(mat):
     dists = pdist(mat, metric='cosine')
-    np.clip(dists, 0, out=dists)
+    np.clip(dists, 0, None, out=dists)
     sim = squareform(1 - dists)
     np.fill_diagonal(sim, 1)
     return sim
